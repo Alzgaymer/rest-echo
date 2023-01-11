44544f527c87dea4478b2cb1eea61921a0e737f9
Slavik <slaw.z2014@gmail.com>
2023-01-11 12:06:25 +0200

    Updated config

--- config/config.go
+++ config/config.go
@@ -1,11 +1,12 @@
 package config
 
 type ConfigDatabase struct {
-	AppName string `env:"APP_NAME" env-default:"ELECTRONICS"`
-	AppEnv  string `env:"APP_ENV" env-default:"DEV"`
-	Port    string `env:"MY_APP_PORT" env-default:"8080"`
-	Host    string `env:"HOST" env-default:"localhost"`
-	LogLvl  string `env:"LOG_LEVEL" env-default:"ERROR"`
+	AppName  string `env:"APP_NAME" env-default:"ELECTRONICS"`
+	AppEnv   string `env:"APP_ENV" env-default:"DEV"`
+	Port     string `env:"MY_APP_PORT" env-default:"8080"`
+	Host     string `env:"HOST" env-default:"localhost"`
+	LogLvl   string `env:"LOG_LEVEL" env-default:"ERROR"`
+	MongoURI string `env:"MONGO_URI" env-default:"mongodb://localhost:27017"`
 }
 
 var configDB ConfigDatabase
