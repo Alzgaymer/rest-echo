99a1dc2dddc0021bbd8079b38b4426ab70218531
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2023-01-15 03:15:49 +0200

    restructured mongo db

--- src/db/mongo/mongodb.go
+++ src/db/mongo/mongodb.go
@@ -4,8 +4,23 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type Mongodb struct {
-	MongoClient *mongo.Client
-	DB          *mongo.Database
-	Collection  *mongo.Collection
+const (
+	_default_db         = "rest-echo-mongo"
+	_default_collection = "item-collction"
+)
+
+type DB interface {
+	New(client *mongo.Client) *Database
+	Col() *mongo.Collection
+}
+type Database struct {
+	db *mongo.Database
+}
+
+func New(client *mongo.Client) *Database {
+	return &Database{db: client.Database(_default_db)}
+}
+
+func (d *Database) Col() *mongo.Collection {
+	return d.db.Collection(_default_collection)
 }
