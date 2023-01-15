4c58bcd3459c5e209f4eb64020b7195ab6e5276a
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2023-01-15 15:05:05 +0200

    Builder update

--- src/builder/builder.go
+++ src/builder/builder.go
@@ -1,10 +1,10 @@
 package builder
 
 import (
-	"context"
 	"log"
 	"server/config"
-	service "server/src/service"
+	mongodb "server/src/db/mongo"
+	"server/src/service"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -16,20 +16,9 @@ type MongoBuilder struct {
 }
 
 func (m MongoBuilder) Build() {
-	var err error
-	m.Service.Mongodb.MongoClient, err = mongo.NewClient(
-		options.Client().ApplyURI(m.Config.MongoURI))
+	client, err := mongo.NewClient(options.Client().ApplyURI(m.Config.MongoURI))
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx := context.Background()
-	err = m.Service.Mongodb.MongoClient.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	m.Service.Mongodb.DB = m.Service.Mongodb.MongoClient.Database("rest-echo")
-	m.Service.Mongodb.Collection = m.Service.Mongodb.DB.Collection("item-collection")
-	if err != nil {
-		log.Fatal(err)
-	}
+	m.Service.Db = mongodb.New(client)
 }
