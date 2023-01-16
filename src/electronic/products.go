6d754ee8e8abe558ad0dba00934f4a0c68cef599
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2023-01-16 16:52:41 +0200

    implemented post method

--- src/electronic/products.go
+++ src/electronic/products.go
@@ -1,11 +1,16 @@
 package electronic
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"server/src/model"
+	"server/src/service"
+	"sync"
 
 	"github.com/labstack/echo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"gopkg.in/go-playground/validator.v9"
 )
 
@@ -17,25 +22,61 @@ func (p *ProductValidator) Validate(i interface{}) error {
 	return p.validator.Struct(i)
 }
 
-var market []*model.Product
+var (
+	lock sync.Mutex
+)
 
 func DeleteByID(c echo.Context) error {
 	return c.JSON(http.StatusOK, "")
 }
 
 func PutByName(c echo.Context) error {
-	//find in cache (in our situation market []*model.Product)
-	//if success skip adding to mongo
-	//if failed find in mongo
-	//if success skip adding
-	//if failed add to mongo
-	//adding
 
 	return c.JSON(http.StatusOK, "")
 }
 
 func PostAdd(c echo.Context) error {
-	return c.JSON(http.StatusOK, "")
+	lock.Lock()
+	defer lock.Unlock()
+
+	var (
+		product model.Product
+		filter  bson.M
+		log     echo.Logger = c.Logger()
+	)
+
+	if err := c.Bind(&product); err != nil {
+		return err
+	}
+	if err := c.Validate(&product); err != nil {
+		return err
+	}
+	product.ID = primitive.NewObjectID()
+	product.CreationTime = product.ID.Timestamp()
+
+	filter = bson.M{"product_name": product.Product_name}
+
+	log.Print("Finding in mongo...")
+
+	res := service.New().Col().FindOne(context.Background(), filter)
+	log.Print(res.Err())
+	log.Print(model.Decode(res))
+	if res.Err() != nil { //if true adding to mongo
+		res, err := service.New().Col().InsertOne(
+			context.Background(),
+			model.MgoToBson(model.ModelToMgo(&product)),
+		)
+		log.Print(res)
+		if err != nil {
+			log.Print(err)
+		}
+	} else {
+		str := fmt.Sprintf("Product: %v, already exist", product)
+		log.Printf(str)
+		return c.JSON(http.StatusOK, str)
+	}
+
+	return c.JSON(http.StatusOK, product)
 }
 
 func GetInit(c echo.Context) error {
@@ -47,7 +88,7 @@ func GetByID(c echo.Context) error {
 }
 
 func GetAll(c echo.Context) error {
-	return c.JSON(http.StatusNotFound, market)
+	return c.JSON(http.StatusNotFound, "")
 }
 
 func AnotherServerMessage(next echo.HandlerFunc) echo.HandlerFunc {
