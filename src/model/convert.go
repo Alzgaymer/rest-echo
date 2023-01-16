e724259ca3e58b4b285f4f144abb34c6c7c179b1
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2023-01-16 16:53:20 +0200

    added decoder function to decode singleresult from mongo

--- src/model/convert.go
+++ src/model/convert.go
@@ -1,6 +1,11 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson"
+import (
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 func MgoToModel(mgo *mgoProduct) *Product {
 	return &Product{
@@ -24,4 +29,24 @@ func MgoToBson(mgo *mgoProduct) *bson.D {
 		{Key: "product_name", Value: mgo.Product_name},
 		{Key: "creation_time", Value: mgo.CreationTime},
 	}
+}
+
+func Decode(res *mongo.SingleResult) *Product {
+	var (
+		err         error
+		bsonproduct bson.M
+		mgoproduct  mgoProduct
+	)
+	res.Decode(&bsonproduct)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	bsonbytes, err := bson.Marshal(bsonproduct)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	bson.Unmarshal(bsonbytes, &mgoproduct)
+	return MgoToModel(&mgoproduct)
 }
