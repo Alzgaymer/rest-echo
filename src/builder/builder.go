20b8536f7d34cf3f4a4cfa3ffd362b0793860fe2
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2023-01-17 17:49:21 +0200

    Added logger to struct

--- src/builder/builder.go
+++ src/builder/builder.go
@@ -2,11 +2,11 @@ package builder
 
 import (
 	"context"
-	"log"
 	"server/config"
 	mongodb "server/src/db/mongo"
 	"server/src/service"
 
+	"github.com/labstack/echo"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -14,13 +14,15 @@ import (
 type MongoBuilder struct {
 	Service *service.Service
 	Config  *config.ConfigDatabase
+	Log     echo.Logger
 }
 
 func (m MongoBuilder) Build() {
 	client, err := mongo.NewClient(options.Client().ApplyURI(m.Config.MongoURI))
 	if err != nil {
-		log.Fatal(err)
+		m.Log.Fatal(err)
 	}
-	client.Connect(context.Background())
+	err = client.Connect(context.Background())
+	m.Log.Print(err)
 	m.Service.Db = mongodb.New(client)
 }
